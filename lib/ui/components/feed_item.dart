8d195420f886bba70f519813e2b877560655311a
Raymond Badoux <info@badouxinternetprojects.com>
2025-02-18 07:46:00 +0100

    extracted function to process feed item

---
+++ lib/ui/components/feed_item.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:web_news/ui/components/feed_item_tile.dart';
+import 'package:web_news/utils/helper_functions.dart' show sanitizeDirtyString;
+
+class FeedItem extends StatelessWidget {
+  final dynamic item;
+  final int index;
+  final String feedType;
+  final String feedContentElement;
+
+  const FeedItem({
+    super.key,
+    required this.item,
+    required this.index,
+    required this.feedType,
+    required this.feedContentElement,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    print(feedType);
+    
+    String feedItemImage = '';
+    String feedItemTitle = '';
+    String feedItemDate = '';
+    String feedItemDescription = '';
+    String feedItemContent = '';
+    String feedItemLink = '';
+
+    if (feedType == 'rss') {
+      // retrieve image
+      if (item['enclosure'] != null && item['enclosure']['url'] != null) {
+        feedItemImage = item['enclosure']['url'].toString();
+      } else if (item['image'] != null && item['image'] != '') {
+        feedItemImage = item['image']['\$t'];
+      } else if (item['media\$thumbnail'] != null) {
+        feedItemImage = item['media\$thumbnail']['url'];
+      } else if (item['media\$content'] != null) {
+        feedItemImage = item['media\$content']['url'];
+      }
+              
+      // retrieve and sanitize title
+      if (item['title'] != null && item['title']?['\$t'] != null) {
+        feedItemTitle = item['title']['\$t'].toString();
+      } else if (item['title'] != null) {
+        feedItemTitle = item['title'].toString();
+      }
+  
+      feedItemTitle = sanitizeDirtyString(feedItemTitle);
+      
+      // retrieve and format date
+      String dateString = '';
+      if (item['pubDate'] != null && item['pubDate']['\$t'] != null) {
+        dateString = item['pubDate']['\$t'];
+        DateTime parsedDate =
+        DateFormat('EEE, dd MMM yyyy HH:mm:ss Z').parse(dateString);
+        feedItemDate =
+          DateFormat('yyyy/MM/dd - HH:mm').format(parsedDate);
+      }
+
+      if (feedContentElement == 'description') {
+        // retrieve and sanitize description
+        if (item['description'] != null && item['description']['\$t'] != null) {
+          feedItemDescription = item['description']['\$t'].toString();
+        } else if (item['description'] != null) {
+          feedItemDescription = item['description'].toString();
+        }
+        
+        feedItemDescription = sanitizeDirtyString(feedItemDescription);
+      } else {
+        // retrieve and sanitize content
+        if (item['content\$encoded'] != null &&
+          item['content\$encoded']['\$t'] != null) {
+          feedItemContent = item['content\$encoded']['\$t'].toString();
+        } else if (item['content\$encoded'] != null) {
+          feedItemContent = item['content\$encoded'].toString();
+        }
+        
+        feedItemContent = sanitizeDirtyString(feedItemContent);
+      }
+      
+      // retrieve and clean link
+      if (item['link'] != null) {
+        if (item['link'] is Map && item['link']['\$t'] != null) {
+          feedItemLink = item['link']['\$t'].toString();
+        } else if (item['link'] is String) {
+          feedItemLink = item['link'].toString()
+            .replaceAll('{__cdata: ', '')
+            .replaceAll('}', '');
+        } 
+      }
+    } else {
+      // retrieve image
+      feedItemImage = (item['link'] as List)
+        .firstWhere((l) => l['rel'] == 'related',
+        orElse: () => {'href': ''})['href'];
+              
+      // retrieve and sanitize title
+      feedItemTitle = item['title']['\$t'] ?? '';
+      feedItemTitle = sanitizeDirtyString(feedItemTitle);
+
+      // retrieve and format date
+      String dateString = '';
+      dateString = item['updated']['\$t'] ?? '';
+      DateTime parsedDate = DateTime.parse(dateString);
+      feedItemDate = DateFormat('yyyy/MM/dd - HH:mm').format(parsedDate);
+          
+      // retrieve and sanitize content
+      feedItemContent = item['content']['\$t'] ?? '';
+      feedItemContent = sanitizeDirtyString(feedItemContent);
+  
+      // retrieve link and clean it a bit
+      feedItemLink = (item['link'] as List)
+        .firstWhere((l) => l.containsKey('href'),
+        orElse: () => {'href': ''})['href'];
+    }
+                
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 4, 16, 0),
+      child: FeedItemTile(
+        feedItemImage: feedItemImage,
+        feedItemTitle: feedItemTitle,
+        feedItemDate: '[${(index + 1).toString()}] [$feedItemDate]',
+        feedItemDescription: feedItemDescription,
+        feedItemContent: feedItemContent,
+        feedItemLink: feedItemLink,
+      ),
+    );
+  }
+}
--- lib/ui/screens/feed_content_screen.dart
+++ lib/ui/screens/feed_content_screen.dart
@@ -4,9 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
-import 'package:intl/intl.dart';
-import 'package:web_news/ui/components/feed_item_tile.dart';
-import 'package:web_news/utils/helper_functions.dart';
+import 'package:web_news/ui/components/feed_item.dart';
 import 'package:web_news/utils/window_top_bar.dart';
 import 'package:xml2json/xml2json.dart';
 
@@ -167,113 +165,11 @@ class _FeedContentScreenState extends State<FeedContentScreen> {
                                   ));
                                 }
                                 
-                                var item = feedItems[index];
-                                String feedItemImage = '';
-                                String feedItemTitle = '';
-                                String feedItemDate = '';
-                                String feedItemDescription = '';
-                                String feedItemContent = '';
-                                String feedItemLink = '';
-
-                                if (feedType == 'rss') {
-                                  // retrieve image
-                                  if (item['enclosure'] != null && item['enclosure']['url'] != null) {
-                                    feedItemImage = item['enclosure']['url'].toString();
-                                  } else if (item['image'] != null && item['image'] != '') {
-                                    feedItemImage = item['image']['\$t'];
-                                  } else if (item['media\$thumbnail'] != null) {
-                                    feedItemImage = item['media\$thumbnail']['url'];
-                                  } else if (item['media\$content'] != null) {
-                                    feedItemImage = item['media\$content']['url'];
-                                  }
-                                          
-                                  // retrieve and sanitize title
-                                  if (item['title'] != null && item['title']?['\$t'] != null) {
-                                    feedItemTitle = item['title']['\$t'].toString();
-                                  } else if (item['title'] != null) {
-                                    feedItemTitle = item['title'].toString();
-                                  }
-                              
-                                  feedItemTitle = sanitizeDirtyString(feedItemTitle);
-                                  
-                                  // retrieve and format date
-                                  String dateString = '';
-                                  if (item['pubDate'] != null && item['pubDate']['\$t'] != null) {
-                                    dateString = item['pubDate']['\$t'];
-                                    DateTime parsedDate =
-                                    DateFormat('EEE, dd MMM yyyy HH:mm:ss Z').parse(dateString);
-                                    feedItemDate =
-                                      DateFormat('yyyy/MM/dd - HH:mm').format(parsedDate);
-                                  }
-
-                                  if (widget.feedContentElement == 'description') {
-                                    // retrieve and sanitize description
-                                    if (item['description'] != null && item['description']['\$t'] != null) {
-                                      feedItemDescription = item['description']['\$t'].toString();
-                                    } else if (item['description'] != null) {
-                                      feedItemDescription = item['description'].toString();
-                                    }
-                                    
-                                    feedItemDescription = sanitizeDirtyString(feedItemDescription);
-                                  } else {
-                                    // retrieve and sanitize content
-                                    if (item['content\$encoded'] != null &&
-                                      item['content\$encoded']['\$t'] != null) {
-                                      feedItemContent = item['content\$encoded']['\$t'].toString();
-                                    } else if (item['content\$encoded'] != null) {
-                                      feedItemContent = item['content\$encoded'].toString();
-                                    }
-                                    
-                                    feedItemContent = sanitizeDirtyString(feedItemContent);
-                                  }
-                                  
-                                  // retrieve and clean link
-                                  if (item['link'] != null) {
-                                    if (item['link'] is Map && item['link']['\$t'] != null) {
-                                      feedItemLink = item['link']['\$t'].toString();
-                                    } else if (item['link'] is String) {
-                                      feedItemLink = item['link'].toString()
-                                        .replaceAll('{__cdata: ', '')
-                                        .replaceAll('}', '');
-                                    } 
-                                  }
-                                } else {
-                                  print(item);
-                                  // retrieve image
-                                  feedItemImage = (item['link'] as List)
-                                    .firstWhere((l) => l['rel'] == 'related',
-                                    orElse: () => {'href': ''})['href'];
-                                          
-                                  // retrieve and sanitize title
-                                  feedItemTitle = item['title']['\$t'] ?? '';
-                                  feedItemTitle = sanitizeDirtyString(feedItemTitle);
-
-                                  // retrieve and format date
-                                  String dateString = '';
-                                  dateString = item['updated']['\$t'] ?? '';
-                                  DateTime parsedDate = DateTime.parse(dateString);
-                                  feedItemDate = DateFormat('yyyy/MM/dd - HH:mm').format(parsedDate);
-                                      
-                                  // retrieve and sanitize content
-                                  feedItemContent = item['content']['\$t'] ?? '';
-                                  feedItemContent = sanitizeDirtyString(feedItemContent);
-                              
-                                  // retrieve link and clean it a bit
-                                  feedItemLink = (item['link'] as List)
-                                    .firstWhere((l) => l.containsKey('href'),
-                                    orElse: () => {'href': ''})['href'];
-                                }
-                                            
-                                return Padding(
-                                  padding: const EdgeInsets.fromLTRB(16, 4, 16, 0),
-                                  child: FeedItemTile(
-                                    feedItemImage: feedItemImage,
-                                    feedItemTitle: feedItemTitle,
-                                    feedItemDate: '[${(index + 1).toString()}] [$feedItemDate]',
-                                    feedItemDescription: feedItemDescription,
-                                    feedItemContent: feedItemContent,
-                                    feedItemLink: feedItemLink,
-                                  ),
+                                return FeedItem(
+                                  item: feedItems[index],
+                                  index: index,
+                                  feedType: feedType,
+                                  feedContentElement: widget.feedContentElement,
                                 );
                               },
                             ),
